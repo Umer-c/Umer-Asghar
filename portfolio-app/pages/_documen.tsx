d06930b7888e2b08291159cb7d8b450f58015e0b
Umer Asghar <73327307+Umer-c@users.noreply.github.com>
2022-07-25 21:53:37 +0200

    Update _documen.tsx

--- portfolio-app/pages/_documen.tsx
+++ portfolio-app/pages/_documen.tsx
@@ -2,9 +2,9 @@ import {Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
   return (
-    <Html className='dark'>
+    <Html>
       <Head />
-      <body>
+      <body className='dark'>
         <Main />
         <NextScript />
       </body>
